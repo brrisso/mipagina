6b7b4ff6d2ba9ec31393899cbaf75feb34742436
brrisso <brunorissoosorio@gmail.com>
2025-05-27 19:44:10 -0400

    link a snake

--- src/paginas/Juegos.js
+++ src/paginas/Juegos.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Juegos() {
   return (
     <div>
       <h1>Juegos</h1>
+      <Link to="/snake" style={{ color: '#fff', marginRight: '15px' }}>Snake</Link>
     </div>
   );
 }
