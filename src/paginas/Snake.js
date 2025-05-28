3f64647df2458081ab2e827d301674641a18df2a
brrisso <brunorissoosorio@gmail.com>
2025-05-28 13:41:32 -0400

    Update Snake.js

--- src/paginas/Snake.js
+++ src/paginas/Snake.js
@@ -45,7 +45,7 @@ const Snake = () => {
     if (gameOver) return;
     const intervalo = setInterval(moverSnake, 200);
     return () => clearInterval(intervalo);
-  }, [snake, direccion, gameOver, moverSnake]);
+  }, [snake, direccion, gameOver]);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
